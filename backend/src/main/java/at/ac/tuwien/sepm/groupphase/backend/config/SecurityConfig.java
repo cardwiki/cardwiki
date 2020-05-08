02469619f623594ee2f85d2fb1a7f9a06bd19ea9
Gittenburg <gittenburg@fsinf.at>
2020-05-08 13:44:12 +0200

    backend: allow CORS credentials for http://localhost:4200

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/config/SecurityConfig.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import at.ac.tuwien.sepm.groupphase.backend.service.impl.CustomOAuth2UserService
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,8 +12,9 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -44,19 +44,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         }
     }
 
+    private final List<String> frontendOrigins = Collections.unmodifiableList(Arrays.asList("http://localhost:4200"));
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        final List<String> permitAll = Collections.unmodifiableList(Collections.singletonList("*"));
-        final List<String> permitMethods = List.of(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
-            HttpMethod.PATCH.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name(), HttpMethod.HEAD.name(),
-            HttpMethod.TRACE.name());
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedHeaders(permitAll);
-        configuration.setAllowedOrigins(permitAll);
-        configuration.setAllowedMethods(permitMethods);
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                final CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedMethods(Arrays.asList("*"));
 
+                // We allow credentials only for whitelisted frontends.
+                if (frontendOrigins.contains(request.getHeader("Origin"))){
+                    config.setAllowedOrigins(frontendOrigins);
+                    config.setAllowCredentials(true);
+                } else {
+                    config.setAllowedOrigins(Arrays.asList("*"));
+                }
+                return config;
+            }
+        };
+    }
 }
