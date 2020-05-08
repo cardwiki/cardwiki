ff90cd4a484bf777f2c159596c951c390ab9daae
Gittenburg <gittenburg@fsinf.at>
2020-05-08 11:16:05 +0200

    add back accidentally removed CORS headers

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/config/SecurityConfig.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/config/SecurityConfig.java
@@ -2,13 +2,21 @@ package at.ac.tuwien.sepm.groupphase.backend.config;
 
 import at.ac.tuwien.sepm.groupphase.backend.service.impl.CustomOAuth2UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -20,9 +28,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.oauth2Login()
-            .userInfoEndpoint().userService(customOAuth2UserService)
-            .and().successHandler(new RefererRedirectionAuthenticationSuccessHandler());
+        http.cors()
+            .and().oauth2Login()
+                .userInfoEndpoint().userService(customOAuth2UserService)
+                .and().successHandler(new RefererRedirectionAuthenticationSuccessHandler());
     }
 
     public class RefererRedirectionAuthenticationSuccessHandler
@@ -34,4 +43,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             setUseReferer(true);
         }
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final List<String> permitAll = Collections.unmodifiableList(Collections.singletonList("*"));
+        final List<String> permitMethods = List.of(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
+            HttpMethod.PATCH.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name(), HttpMethod.HEAD.name(),
+            HttpMethod.TRACE.name());
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedHeaders(permitAll);
+        configuration.setAllowedOrigins(permitAll);
+        configuration.setAllowedMethods(permitMethods);
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
