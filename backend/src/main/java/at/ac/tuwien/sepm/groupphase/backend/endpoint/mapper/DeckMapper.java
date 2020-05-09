cacb202772106c66d5f4677693225837d3dac678
4k3r <37218789+4k3r@users.noreply.github.com>
2020-05-09 21:57:35 +0200

    Added create and search methods; updated DeckMapper

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/mapper/DeckMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/mapper/DeckMapper.java
@@ -1,11 +1,18 @@
 package at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper;
 
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.DeckDto;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.DeckInputDto;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Deck;
+import at.ac.tuwien.sepm.groupphase.backend.entity.DeckCategoryId;
 import org.mapstruct.Mapper;
 
 @Mapper
-public interface DeckMapper {
-    DeckDto deckToDeckDto(Deck deck);
-    Deck deckDtoToDeck(DeckDto deckDto);
+public abstract class DeckMapper {
+    public abstract DeckDto deckToDeckDto(Deck deck);
+
+    public abstract Deck deckInputDtoToDeck(DeckInputDto deckInputDto);
+
+    public Long test(DeckCategoryId deckCategoryId) {
+        return deckCategoryId.getCategoryId();
+    }
 }
