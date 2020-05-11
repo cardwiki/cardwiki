15a88f09e883c2cafbd9a0859581f1c939edc0ed
Gittenburg <gittenburg@fsinf.at>
2020-05-11 11:23:31 +0200

    fix Test import

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/AuthEndpointTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/AuthEndpointTest.java
@@ -1,6 +1,6 @@
 package at.ac.tuwien.sepm.groupphase.backend.integrationtest;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
