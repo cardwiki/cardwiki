a7ab9b8a6883c937e154e74c30b8342a2eaf55cd
Gittenburg <gittenburg@fsinf.at>
2020-05-18 14:13:57 +0200

    do not use UserInputDto in UserEndpointTest

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/UserEndpointTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/UserEndpointTest.java
@@ -1,8 +1,9 @@
 package at.ac.tuwien.sepm.groupphase.backend.integrationtest;
 
 import at.ac.tuwien.sepm.groupphase.backend.basetest.TestDataGenerator;
-import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.UserInputDto;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -26,14 +27,21 @@ public class UserEndpointTest extends TestDataGenerator {
 
     @Test
     public void createUser() throws Exception {
+        JsonNode body = objectMapper.createObjectNode();
+
+        ObjectNode input = objectMapper.createObjectNode();
+        input.put("username", "test");
+        input.put("description", "example");
+        input.put("admin", false);
+
         mvc.perform(post("/api/v1/users")
             .with(mockLogin(USER_ROLES, "123"))
-            .contentType("application/json").content(objectMapper.writeValueAsString(new UserInputDto("test", "example", false))))
-            .andExpect(status().is(201))
-            .andExpect(jsonPath("$.username").value("test"))
-            .andExpect(jsonPath("$.description").value("example"))
-            .andExpect(jsonPath("$.createdAt", validIsoDateTime()))
-            .andExpect(jsonPath("$.updatedAt", validIsoDateTime()));
+            .contentType("application/json").content(input.toString()))
+        .andExpect(status().is(201))
+        .andExpect(jsonPath("$.username").value("test"))
+        .andExpect(jsonPath("$.description").value("example"))
+        .andExpect(jsonPath("$.createdAt", validIsoDateTime()))
+        .andExpect(jsonPath("$.updatedAt", validIsoDateTime()));
         // TODO: test more thoroughly
     }
 }
