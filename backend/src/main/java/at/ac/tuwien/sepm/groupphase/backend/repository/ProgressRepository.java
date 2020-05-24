84efe841e13800d7d3990229fce5a187faa5a326
4k3r <37218789+4k3r@users.noreply.github.com>
2020-05-24 20:07:08 +0200

    add nextCards repository function

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/ProgressRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/ProgressRepository.java
@@ -1,9 +1,17 @@
 package at.ac.tuwien.sepm.groupphase.backend.repository;
 
+import at.ac.tuwien.sepm.groupphase.backend.entity.Card;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Progress;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProgressRepository extends JpaRepository<Progress,Progress.Id> {
+    @Query("select c from Card c left join Progress p on c.id = p.id.card.id where c.deck.id=:deckId")
+    List<Card> findNextCards(@Param("deckId") Long deckId, Pageable pageable);
 }
