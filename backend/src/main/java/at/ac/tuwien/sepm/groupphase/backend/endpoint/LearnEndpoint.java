f627a011007c6be845a43bf609c5d3e055297f03
4k3r <37218789+4k3r@users.noreply.github.com>
2020-05-24 10:12:34 +0200

    Changed dependency injection

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/LearnEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/LearnEndpoint.java
@@ -4,19 +4,26 @@ import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.AttemptInputDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.CardDetailsDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.CardMapper;
 import at.ac.tuwien.sepm.groupphase.backend.service.LearnService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.lang.invoke.MethodHandles;
 
 @RestController
 @RequestMapping(value = "/api/v1/learn")
 public class LearnEndpoint {
-    @Autowired
-    private LearnService learnService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private final LearnService learnService;
+    private final CardMapper cardMapper;
 
     @Autowired
-    private CardMapper cardMapper;
+    public LearnEndpoint(LearnService learnService, CardMapper cardMapper) {
+        this.learnService = learnService;
+        this.cardMapper = cardMapper;
+    }
 
     @PostMapping("/attempt")
     public void attempt(@Valid @RequestBody AttemptInputDto attempt){
