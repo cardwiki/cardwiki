10a397e5d0cca520f0ec74120a58ea12df718a51
Tin Pecirep <tin.pecirep@gmail.com>
2020-05-27 12:37:14 +0200

    make flip() function in deck preview actually useful and use modulo to cycle through cards endlessly

--- frontend/src/app/components/deck-preview/deck-preview.component.ts
+++ frontend/src/app/components/deck-preview/deck-preview.component.ts
@@ -17,9 +17,9 @@ export class DeckPreviewComponent implements OnInit {
   handleKeyboardEvent(event: KeyboardEvent) {
     if (event.key === 'ArrowRight') this.nextCard();
     else if (event.key === 'ArrowLeft') this.previousCard();
-    else if (event.key === 'ArrowUp') this.flipped = true;
-    else if (event.key === 'ArrowDown') this.flipped = false;
-    else if (event.key === ' ') this.flipped = !this.flipped;
+    else if (event.key === 'ArrowUp') this.flipTo('front');
+    else if (event.key === 'ArrowDown') this.flipTo('back');
+    else if (event.key === ' ') this.flip();
   }
 
   deck: Deck;
@@ -46,21 +46,24 @@ export class DeckPreviewComponent implements OnInit {
     });
   }
 
-  flip(to: boolean): void {
-    //this.flipped = !this.flipped; //doesn't work??
-    this.flipped = to;
+  flipTo(to: 'front' | 'back'): void {
+    this.flipped = to === 'back';
+  }
+
+  flip(): void {
+    this.flipped = !this.flipped;
   }
 
   nextCard(): void {
     this.flipped = false;
     const nextCardId = this.cards.indexOf(this.currentcard) + 1;
-    this.currentcard = this.cards[nextCardId < this.cards.length ? nextCardId : 0];
+    this.currentcard = this.cards[nextCardId % this.cards.length];
   }
 
   previousCard(): void {
     this.flipped = false;
     const previousCardId = this.cards.indexOf(this.currentcard) - 1;
-    this.currentcard = this.cards[previousCardId >= 0 ? previousCardId : this.cards.length-1];
+    this.currentcard = this.cards[(previousCardId + this.cards.length) % this.cards.length];
   }
 
 }
