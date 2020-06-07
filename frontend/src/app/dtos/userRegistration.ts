9f785f9f08bf81c219b00695e884106ebde2b51b
Gittenburg <gittenburg@fsinf.at>
2020-06-07 09:01:26 +0200

    reset UserRegistration dto in the frontend

--- frontend/src/app/dtos/userRegistration.ts
+++ frontend/src/app/dtos/userRegistration.ts
@@ -1,6 +1,9 @@
 export class UserRegistration {
     constructor(
         public username: string,
-        public description: string) {
+        public description: string,
+        public createdAt: Date,
+        public updatedAt: Date,
+        public isAdmin: boolean) {
     }
 }
