819a769c8547f4e6fca9ccedb199bb162a41aa20
A_A <21040751+Otto-AA@users.noreply.github.com>
2020-06-27 20:22:08 +0200

    Remove unused category components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -15,8 +15,6 @@ import { DeckPreviewComponent } from './components/deck-preview/deck-preview.com
 import {CategoryUpdateComponent} from './components/category/category-update/category-update.component';
 import {CategoryListComponent} from './components/category/category-list/category-list.component';
 import {CategoryDetailsComponent} from './components/category/category-details/category-details.component';
-import {CategorySubcategoriesComponent} from './components/category/category-subcategories/category-subcategories.component';
-import {CategoryDecksComponent} from './components/category/category-decks/category-decks.component';
 
 import { MarkdownSyntaxComponent } from './components/help/markdown-syntax/markdown-syntax.component';
 import {ProfileComponent} from "./components/profile/profile.component";
@@ -29,9 +27,7 @@ const routes: Routes = [
   {path: 'categories', component: CategoryListComponent},
   {path: 'categories/new', canActivate: [AuthGuard], component: CategoryCreateComponent},
   {path: 'categories/:id', component: CategoryDetailsComponent},
-  {path: 'categories/:id/decks', component: CategoryDecksComponent},
   {path: 'categories/:id/edit', canActivate: [AuthGuard], component: CategoryUpdateComponent},
-  {path: 'categories/:id/subcategories', component: CategorySubcategoriesComponent},
   {path: 'decks/:id', component: DeckViewComponent},
   {path: 'decks/:id/cards/new', canActivate: [AuthGuard], component: CardCreateComponent},
   {path: 'decks/:id/edit', canActivate: [AuthGuard], component: DeckEditComponent},
