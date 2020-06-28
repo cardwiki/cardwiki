496d55b4726bd822e22cf07b5fe64ed782def984
4k3r <37218789+4k3r@users.noreply.github.com>
2020-06-28 18:40:03 +0200

    fixes a bug that caused a 400 response on card deletion
    The `@AllArgsConstructor` annotation caused the validation to fail
    for valid request.

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/ReasonDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/ReasonDto.java
@@ -1,13 +1,11 @@
 package at.ac.tuwien.sepm.groupphase.backend.endpoint.dto;
 
 import at.ac.tuwien.sepm.groupphase.backend.entity.Revision;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.Size;
 
 @Data
-@AllArgsConstructor
 public class ReasonDto {
     @Size(max = Revision.MAX_MESSAGE_SIZE)
     private String message;
