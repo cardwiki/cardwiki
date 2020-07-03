20f7c1eec359933eb9cc7b744b577f6adcfca17c
A_A <21040751+Otto-AA@users.noreply.github.com>
2020-07-03 17:21:39 +0200

    Rename frontend project to cardwiki

--- frontend/e2e/src/app.e2e-spec.ts
+++ frontend/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to sepm-group-phase!');
+    expect(page.getParagraphText()).toEqual('Welcome to cardwiki!');
   });
 });
