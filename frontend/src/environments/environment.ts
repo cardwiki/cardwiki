e34cf399da214a6b78816cbc6b5cb0695b24f12e
A_A <21040751+Otto-AA@users.noreply.github.com>
2020-07-21 10:26:00 +0200

    Make backend URL configurable in frontend.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,8 +1,11 @@
+import { config } from './config';
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  ...config,
   production: false
 };
 
