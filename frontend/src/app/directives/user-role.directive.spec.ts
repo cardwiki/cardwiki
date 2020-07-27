7f48a16895a53c1278999d9263ce2c7c4f19678f
Gittenburg <gittenburg@fsinf.at>
2020-07-27 13:20:31 +0200

    Remove unused spec files.

--- frontend/src/app/directives/user-role.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UserRoleDirective } from './user-role.directive';
-
-describe('UserRoleDirective', () => {
-  it('should create an instance', () => {
-    const directive = new UserRoleDirective();
-    expect(directive).toBeTruthy();
-  });
-});
