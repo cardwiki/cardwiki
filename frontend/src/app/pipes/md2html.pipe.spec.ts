7c8f5cac078f08393d6678d42d908f4e6829fd15
A_A <21040751+Otto-AA@users.noreply.github.com>
2020-07-27 16:30:34 +0200

    Remove unused testing frameworks karma and protractor

--- frontend/src/app/pipes/md2html.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Md2htmlPipe } from './md2html.pipe';
-
-describe('Md2htmlPipe', () => {
-  it('create an instance', () => {
-    const pipe = new Md2htmlPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
